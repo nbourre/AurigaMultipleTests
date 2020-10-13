f3dd636981fcb2be0867f71811445225a9188fa0
Nicolas Bourré <nbourrereg@hotmail.com>
2020-10-13 10:55:08 -0400

    Added onboard temperature sensor reading

--- AurigaMultipleTests/AurigaMultipleTests.ino
+++ AurigaMultipleTests/AurigaMultipleTests.ino
@@ -31,8 +31,6 @@
 // on-board LED ring, at PORT0 (onboard)
 MeRGBLed led( 0, LEDNUM );
 
-
-
 #else
 
 #define LEDNUM  14
@@ -45,6 +43,9 @@ MeRGBLed led( PORT_3 );
 MeSoundSensor mySound(PORT_6);
 MeGyro gyro(0, 0x69);
 
+const int tempPin = A0;
+int tempSensorValue = 0;
+float tempOutput = 0;
 
 float j, f, k;
 int serial_refresh_rate = 200;
@@ -60,6 +61,7 @@ void setup()
     led.setpin( 44 );
 #endif
   Serial.begin(115200);
+  
   gyro.begin();
 
   ct = millis();
@@ -73,10 +75,11 @@ void loop()
   dt = ct - pt;
   pt = ct;
   
-  color_loop();
+  led_loop_task();
   measure_sound();
-  gyro_test();
   
+  //gyro_task();
+  temp_task();  
   show_text();
 }
 
@@ -100,7 +103,7 @@ void measure_sound() {
 int gyro_acc = 0;
 int gyro_int = 10;
 
-void gyro_test() {
+void gyro_task() {
   gyro_acc += dt;
 
   if (gyro_acc < gyro_int) return;
@@ -117,22 +120,29 @@ void gyro_test() {
 
 }
 
+
 void show_text() {
   serial_acc += dt;
   
   if (serial_acc < serial_refresh_rate) return;
 
-  serial_acc = 0;
+  serial_acc = serial_acc % serial_refresh_rate;
 
+  // Resetting sound values
   sound_avg /= sound_cnt;
-      
-  Serial.print("Sound = ");
-  Serial.println(sound_avg);
   sound_rst = 1;
+
   
+  Serial.print("Sound = ");
+  Serial.print(sound_avg);
+
+  Serial.print("\tTemperature = ");
+  Serial.print(tempOutput);
+
+  Serial.println();
 }
 
-void color_loop()
+void led_loop_task()
 {
   for (uint8_t t = 0; t < LEDNUM; t++ )
   {
@@ -146,4 +156,40 @@ void color_loop()
   j += random(1, 6) / 6.0;
   f += random(1, 6) / 6.0;
   k += random(1, 6) / 6.0;
+}
+
+/**
+Temperature values
+Src : https://github.com/search?q=TERMISTORNOMINAL+auriga&type=code
+*/
+
+const int16_t TEMPERATURENOMINAL     = 25;    //Nominl temperature depicted on the datasheet
+const int16_t SERIESRESISTOR         = 10000; // Value of the series resistor
+const int16_t BCOEFFICIENT           = 3380;  // Beta value for our thermistor(3350-3399)
+const int16_t TERMISTORNOMINAL       = 10000; // Nominal temperature value for the thermistor
+
+float calculate_temp(int16_t In_temp)
+{
+  float media;
+  float temperatura;
+  media = (float)In_temp;
+  // Convert the thermal stress value to resistance
+  media = 1023.0 / media - 1;
+  media = SERIESRESISTOR / media;
+  //Calculate temperature using the Beta Factor equation
+
+  temperatura = media / TERMISTORNOMINAL;              // (R/Ro)
+  temperatura = log(temperatura); // ln(R/Ro)
+  temperatura /= BCOEFFICIENT;                         // 1/B * ln(R/Ro)
+  temperatura += 1.0 / (TEMPERATURENOMINAL + 273.15);  // + (1/To)
+  temperatura = 1.0 / temperatura;                     // Invert the value
+  temperatura -= 273.15;                               // Convert it to Celsius
+  return temperatura;
+}
+
+
+
+float temp_task() {
+    tempSensorValue = analogRead(tempPin);
+    tempOutput = calculate_temp(tempSensorValue);
 }
